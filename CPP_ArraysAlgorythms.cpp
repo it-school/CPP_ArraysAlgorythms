3dc8a8ceba34a168cbc7b54cb689c0d4cdf91d69
vencedor <it@osvita.od.ua>
2022-11-19 16:29:03 +0200

    Syntax enhacements, November 2022

--- CPP_ArraysAlgorythms.cpp
+++ CPP_ArraysAlgorythms.cpp
@@ -5,7 +5,8 @@ using namespace std;
 
 int main()
 {
-	cout << "Array algorythms: linear search" << endl;
+	setlocale(LC_ALL, "ru");
+	cout << "Array algorythms: linear search, sorts, i.e." << endl;
 	const int N = 20;
 	int array[N];
 
@@ -15,20 +16,32 @@ int main()
 		cout << setw(4) << array[i];
 	}
 
+	// линейный поиск (linear search)
 	int number = 5, position = -1;
-
 	for (int i = 0; i < N; i++)
 	{
 		if (array[i] == number)
 		{
 			position = i;
-			break;  // добавляем для поиска первого вхождения
+			break;  // добавляем для поиска только первого вхождения
 		}
 	}
-	cout << endl << "Position of number " << number << " is " << position;
+	cout << endl << "Position of number " << number << " is " << position << endl;
 
+	// Поиск в массиве MIN и MAX значений и их индексов
+	int indexMin = 0, indexMax = 0;
+	for (int i = 1; i < N; i++)
+	{
+		if (array[i] < array[indexMin])
+			indexMin = i;
+		else
+			if (array[i] > array[indexMax])
+				indexMax = i;
+	}
+	cout << endl << "Минимальный элемент на ходится на позиции: " << indexMin << ", его значение: " << array[indexMin];
+	cout << endl << "Максимальный элемент на ходится на позиции: " << indexMax << ", его значение: " << array[indexMax] << endl;
 
-	// Сортировка поиском минимального 
+	// Сортировка поиском (выбором) минимального (selection sort)
 	int imin = 0, temp;
 
 	// N-1 * N/2 - количество операций
@@ -52,7 +65,7 @@ int main()
 		}
 	}
 
-	// Пузырьковая сортировка 
+	// Пузырьковая сортировка (bubble sort)
 	int n = 0;
 	bool isOrdered = true;
 	for (size_t j = 0; j < N - 1; j++)
@@ -81,10 +94,41 @@ int main()
 	}
 	cout << endl << "Total operations: " << n;
 
+	// Двоичный (бинарный) поиск в отсортированном массиве 
+	int steps = ceil(log2(n));	// кол-во шагов для поиска
+	int middle = n / 2;			// середина для старта поиска
+	number = 555;				// искомое значение
+	position = -1;				// позиция найденного элемента или -1, если не найден
+
+	cout << endl << steps;
+	for (size_t i = 1; i <= n; i++)
+	{
+		if (array[middle] == number)
+		{
+			number = middle;
+			break;
+		}
+		else
+		{
+			if (array[middle] > number)
+			{
+				middle = middle / 2; // go left
+			}
+			else
+			{
+				middle = middle + middle / 2; // go right
+			}
+
+			// middle = array[middle] > value ? middle / 2 : middle + middle / 2;
+		}
+	}
+	cout << endl << "Индекс для значения " << number << " => " << position;
+
+
 
 	// Практический пример
 	n = 0;
-	setlocale(LC_ALL, "ru");
+
 	cout << endl << endl << "Известны показания термометра за год. Найти 3 самых теплых и 3 самых холoдных дня в году:" << endl;
 	const int DAYS = 365;
 	int year[DAYS];
@@ -129,9 +173,12 @@ int main()
 	*/
 
 	// Без сортировки - обычный поиск минимума и максимума
+
+	cout << endl << endl << "Пары температур для самых 3 холодных и самых тёплых дней года:" << endl;
+
 	n = 0;
 	int iMax = 0, iMin = 0;
-	for (int i = 0; i < DAYS; i++)
+	for (int i = 1; i < DAYS; i++)
 	{
 		if (year[i] < year[iMin])
 			iMin = i;
@@ -139,11 +186,13 @@ int main()
 			iMax = i;
 		n++;
 	}
-	cout << endl << year[iMin] << endl;
-	cout << endl << year[iMax] << endl;
 
-	int iMax2 = 0, iMin2 = 0;
-	for (int i = 0; i < DAYS; i++)
+	cout << endl << "1 минимальная температура года: " << year[iMin];
+	cout << endl << "1 максимальная температура года: " << year[iMax];
+
+	int iMax2 = iMax == 0 ? 1 : 0;		// избегаем старта с уже найденного максимума
+	int iMin2 = iMin == 0 ? 1 : 0;		// избегаем старта с уже найденного минимума
+	for (int i = 1; i < DAYS; i++)
 	{
 		if (year[i] < year[iMin2] && year[i] > year[iMin])
 			iMin2 = i;
@@ -151,11 +200,12 @@ int main()
 			iMax2 = i;
 		n++;
 	}
-	cout << endl << year[iMin2] << endl;
-	cout << endl << year[iMax2] << endl;
+	cout << endl << "2 минимальная температура года: " << year[iMin2];
+	cout << endl << "2 максимальная температура года: " << year[iMax2];
 
-	int iMax3 = 0, iMin3 = 0;
-	for (int i = 0; i < DAYS; i++)
+	int iMax3 = iMax == 1 || iMax2 == 1 ? 2 : iMax == 0 || iMax2 == 0 ? 1 : 0;  // избегаем старта с уже найденных максимумов
+	int iMin3 = iMin == 1 || iMin2 == 1 ? 2 : iMin == 0 || iMin2 == 0 ? 1 : 0;  // избегаем старта с уже найденных минимумов
+	for (int i = 1; i < DAYS; i++)
 	{
 		if (year[i] < year[iMin3] && year[i] > year[iMin2])
 			iMin3 = i;
@@ -163,7 +213,7 @@ int main()
 			iMax3 = i;
 		n++;
 	}
-	cout << endl << year[iMin3] << endl;
-	cout << endl << year[iMax3] << endl;
-	cout << endl << "Total operations: " << n;
+	cout << endl << "3 минимальная температура года: " << year[iMin3];
+	cout << endl << "3 максимальная температура года: " << year[iMax3];
+	cout << endl << endl << "Total operations: " << n << endl;
 }
